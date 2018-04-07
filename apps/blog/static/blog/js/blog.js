0c9456b5d89c5dabdc0879524f0a0f0b36bfe5bb
RenKang <rk19931211@hotmail.com>
2018-04-07 18:29:21 +0800

    添加全文搜索，和联想输入

--- apps/blog/static/blog/js/blog.js
+++ apps/blog/static/blog/js/blog.js
@@ -29,33 +29,6 @@ jQuery(document).ready(function($) {
         $(this).children('span').fadeOut(200);
     });
 
-    var data = [
-        { "value": "1", "label": "one" },
-        { "value": "2", "label": "two" },
-        { "value": "3", "label": "three" },
-        { "value": "4", "label": "four" },
-        { "value": "5", "label": "five" },
-        { "value": "6", "label": "six" }
-    ];
-    $('#search-input').autocompleter({
-        source:'http://localhost:8000',
-        limit:5,
-        cache:true,
-        combine:function (args) {
-            // 可以额外给服务器发送参数
-            return {
-                'q':args.query,
-                'count':args.limit
-            };
-        },
-        callback:function (value, index, object) {
-            console.log(
-                'Value ' + value + ' are selected (with index ' + index + ').'
-            );
-            console.log(object);
-        }
-    });
-
     $("#tagcloud").windstagball({
         radius:100,
         speed:0.5
