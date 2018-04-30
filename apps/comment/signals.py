df042190dc96b00596d822bb78b4eb8fc5399fc2
nico <jennei@hotmail.com>
2018-04-30 13:42:35 +0800

    add comment signals

--- apps/comment/signals.py
+++ apps/comment/signals.py
@@ -4,7 +4,18 @@ __date__ = '2018/4/22 12:32'
 
 
 from django.dispatch import receiver, Signal
+from comment.models import Comment
 
 
-post_like = Signal(providing_args=['comment_obj'])
-post_dislike = Signal(providing_args=['comment_obj'])
+post_like = Signal(providing_args=['comment_obj', 'content_type', 'object_id'])
+post_comment = Signal(providing_args=['comment_obj', 'content_type', 'object_id'])
+
+
+# @receiver(post_like, sender=Comment)
+# def handler_post_like(sender, comment_obj, content_type, object_id, **kwargs):
+#     print("post_like sender:%s, comment:%s, content_type:%s, object_id:%s"%(sender, comment_obj, content_type, object_id))
+#
+#
+# @receiver(post_comment, sender=Comment)
+# def handler_post_comment(sender, comment_obj, content_type, object_id, **kwargs):
+#     print("post_comment sender:%s, comment:%s, content_type:%s, object_id:%s"%(sender, comment_obj, content_type, object_id))
