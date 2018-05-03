24e22d050c9a33b81c0e671a6d63ac6ab8de7964
nico <jennei@hotmail.com>
2018-05-03 13:21:47 +0800

    remove kindeditor ke-container style width

--- apps/comment/static/comment/kindeditor/config.js
+++ apps/comment/static/comment/kindeditor/config.js
@@ -2,9 +2,11 @@ KindEditor.ready(function (K) {
     window.editor = K.create("#comment-widget", {
         "afterCreate": function () {
             this.sync();
+            this.removeAttr('style');
         },
         "afterChange": function () {
             this.sync();
+            $('.ke-container').removeAttr('style');
         },
         "afterBlur": function () {
             this.sync();
