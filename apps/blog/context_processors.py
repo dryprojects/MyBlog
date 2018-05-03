a5a8fd1b956ce0ff2b6e221adaeeb28d8760f8ed
nico <jennei@hotmail.com>
2018-05-04 01:36:57 +0800

    add blog mptt category

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -7,7 +7,7 @@
 @time:      2018/05/03 
 """ 
 
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Category
 
 
 def post_extra(request):
@@ -18,7 +18,14 @@ def post_extra(request):
     #所有博文的标签
     post_tag_list = Tag.objects.all()
     context['post_tag_list'] = post_tag_list
-    #好评最多的
-
+    #所有的博文分类
+    cts = Category.objects.all()
+    context['post_category_list'] = cts
+    #好评最多的 喜欢数排序
+    #热门博文 浏览数排序
+    #博主推荐
+    #友情链接
+    #公告
+    #博主信息
 
     return context
