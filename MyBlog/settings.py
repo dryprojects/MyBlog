a9b4653928f28ea1a7e5bee84699d5106b178c9e
303288346 <rk19931211@hotmail.com>
2018-06-17 13:25:28 +0800

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -20,8 +20,7 @@ from celery.schedules import crontab
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
-sys.path.insert(1, os.path.join(BASE_DIR, 'thirdparty_apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))                      #这里需要把项目根目录下的apps设置为包搜索目录
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
