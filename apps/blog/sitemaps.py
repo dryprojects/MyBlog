9f4d760f9fb59c973b9c094d642da12d8a4b9c93
nico <jennei@hotmail.com>
2018-06-17 15:35:32 +0800

    add blog site map

---
+++ apps/blog/sitemaps.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 -*-
+
+""" 
+@author:    nico 
+@file:      sitemaps.py 
+@time:      2018/06/17 
+""" 
+
+from django.contrib.sitemaps import Sitemap
+
+from blog.models import Post
+
+
+class BlogSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+        return Post.objects.filter(status='published', type='post')
+
+    def lastmod(self, obj):
+        return obj.published_time
