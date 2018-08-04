36aeee69f31b4c946ebb698ce40bfd6ec4f76d8b
nico <jennei@hotmail.com>
2018-08-04 23:33:23 +0800

    添加用户接口过滤器

--- apps/bloguser/api/views.py
+++ apps/bloguser/api/views.py
@@ -8,9 +8,10 @@
 """ 
 
 from bloguser import models
-from bloguser.api import serializers, throttling, filters
+from bloguser.api import serializers, throttling, filters as user_filters
 
 from rest_framework import viewsets, throttling as rest_throttling
+from django_filters import rest_framework as filters
 
 from dry_rest_permissions.generics import DRYPermissions
 
@@ -19,6 +20,8 @@ class UserViewset(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     permission_classes = (DRYPermissions, )
     throttle_classes = (throttling.UserRateThrottle, )
+    filter_backends = (filters.DjangoFilterBackend, user_filters.UserFilterBackend)
+    filter_class = user_filters.UserFilter
 
     def get_serializer_class(self):
         if self.action in ["list"]:
