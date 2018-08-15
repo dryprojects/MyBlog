c1506c4d422e5aa4a3e79ea6e6130718a5cc2b98
nico <jennei@hotmail.com>
2018-08-16 00:58:14 +0800

    修改：     apps/bloguser/api/views.py

--- apps/bloguser/api/views.py
+++ apps/bloguser/api/views.py
@@ -10,11 +10,12 @@
 from django.urls.exceptions import NoReverseMatch
 
 from rest_framework import viewsets, throttling as rest_throttling
-from rest_framework import mixins, permissions, reverse
+from rest_framework import mixins, permissions, reverse, response, status
 from django_filters import rest_framework as filters
 
 from dry_rest_permissions.generics import DRYPermissions
 from djoser import views
+from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
 
 from bloguser import models
 from bloguser.api import serializers, throttling, filters as user_filters
