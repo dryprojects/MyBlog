f7b90fe28d007db4089f179e78d02b24f6370ea1
nico <jennei@hotmail.com>
2018-08-24 20:53:03 +0800

    集成sentry

--- apps/bloguser/tests.py
+++ apps/bloguser/tests.py
@@ -1,3 +1,23 @@
+import os
+import sys
+
 from django.test import TestCase
 
+
+project_base = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+sys.path.append(project_base)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'MyBlog.settings'
+
+import django
+
+django.setup()
+
 # Create your tests here.
+from raven.contrib.django.raven_compat.models import client
+
+try:
+    a = 1/0
+except ZeroDivisionError:
+    client.captureException()
